47853834abb55ec8f7bf22fe70b7b60a8def499a
Meddy Menzikoff <meddy@MacBook-Pro-de-Meddy.local>
2016-11-20 16:11:41 +0100

    Fix: Dust Detector

--- TempHumi.ino
+++ TempHumi.ino
@@ -11,11 +11,21 @@
 #define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
 DHT dht(DHTPIN, DHTTYPE);
 
+//Air Quality
 AirQuality airqualitysensor;
 int current_quality =-1;
+
+//Dust Sensor
+int dust_pin = 8;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 30000;//sampe 30s ;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
 
 void setup() {
   Serial.begin(9600);
@@ -24,6 +34,10 @@ void setup() {
   airqualitysensor.init(14);
   dht.begin();
   //pinMode(sensorPin, INPUT);
+
+  pinMode(dust_pin,INPUT);
+  starttime = millis();//get the current time;
+  
 }
 
 void loop() {
@@ -67,7 +81,7 @@ void getAirQuality(){
     current_quality=airqualitysensor.slope();
   
     Serial.print("Air Quality: ");
-    Serial.print(current_quality);
+    Serial.print(airqualitysensor.first_vol);
     Serial.print(" ");
     if (current_quality==0)
         Serial.println("Emergency   ");
@@ -80,7 +94,25 @@ void getAirQuality(){
 }
 
 void getDustDectector(){
-  
+  // Checking Dust Sensor
+  duration = pulseIn(dust_pin, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+
+  if ((millis()-starttime) > sampletime_ms) //if the sample time == 30s
+  {
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    /*Serial.print(lowpulseoccupancy);
+    Serial.print(",");
+    Serial.print(ratio);
+    Serial.print(",");
+    Serial.println(concentration);*/
+    Serial.print("Particles:");
+    Serial.print("    ");
+    Serial.println(concentration);
+    lowpulseoccupancy = 0;
+    starttime = millis();
+  }
 }
 
 ISR(TIMER2_OVF_vect)
