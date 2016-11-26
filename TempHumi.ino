e5345de82b8ea4bc636dfac783727f252f60b99c
Meddy Menzikoff <m.menzikoff@oneclickflare.fr>
2016-11-26 18:14:02 +0100

    Feat: Affichage données.

--- TempHumi.ino
+++ TempHumi.ino
@@ -3,19 +3,24 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include "Arduino.h"
+#include "U8glib.h"
 
 #define DHTPIN 2  //PIN Numérique du capteur TEMP/HUMI
-#define MQ135PIN A7//Air Quality Sensor 
+#define MQ135PIN A7//Air fQuality Sensor  
 
 // Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 DHT dht(DHTPIN, DHTTYPE);
+float tempRes = 0;
+float temp = 0;
+float hum = 0;
 
 //Air Quality
 AirQuality airqualitysensor;
 int current_quality =-1;
+int air_quality_value = 0;
 
 //Dust Sensor
 int dust_pin = 8;
@@ -31,9 +36,20 @@ float concentration = 0;
 #define yellow_led_pin 5 
 #define blue_led_pin 6  
 
+//Display
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST);  
+const int BuffSize= 9; 
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting");
+  
+  u8g.firstPage(); 
+  do {  
+    u8g.setFont(u8g_font_profont15);
+    u8g.drawStr(0,14, "Init...");  
+  } while( u8g.nextPage() ); 
+     
+  //Serial.begin(9600);
+  //Serial.println("Starting");
 
   airqualitysensor.init(14);
   dht.begin();
@@ -56,6 +72,8 @@ void loop() {
   getDHTValues(); 
   getAirQuality();
   getDustDectector();
+
+  displayResults();
   
 }
 
@@ -65,47 +83,49 @@ void getDHTValues(){
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
 
-
   if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
+    //Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
+  hum = h;
+  temp = t;
   // Compute heat(chaleur ressentie) index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
+  tempRes = (int)hic;
 
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.println(" *C ");
+//  Serial.print("Humidity: ");
+//  Serial.print(h);
+//  Serial.print(" %\t");
+//  Serial.print("Temperature: ");
+//  Serial.print(t);
+//  Serial.print(" *C ");
+//  Serial.print("Heat index: ");
+//  Serial.print(hic);
+//  Serial.println(" *C ");
 }  
 
 void getAirQuality(){
     current_quality=airqualitysensor.slope();
+    air_quality_value = airqualitysensor.first_vol;
   
-    Serial.print("Air Quality: ");
-    Serial.print(airqualitysensor.first_vol);
-    Serial.print(" ");
+//    Serial.print("Air Quality: ");
+//    Serial.print(airqualitysensor.first_vol);
+//    Serial.print(" ");
     if (current_quality==0){
-      Serial.println("Emergency   ");
+      //Serial.println("Emergency   ");
       lightRedLed();
     }
     else if (current_quality==1){
-      Serial.println("Hi Pollution");
+      //Serial.println("Hi Pollution");
       lightYellowLed();
     }
     else if (current_quality==2){
-      Serial.println("Lo Pollution");
+      //Serial.println("Lo Pollution");
       lightBlueLed(); 
     }
     else if (current_quality ==3){
-      Serial.println("Fresh air   ");
+      //Serial.println("Fresh air   ");
       lightBlueLed(); 
     }
 }
@@ -124,9 +144,9 @@ void getDustDectector(){
     Serial.print(ratio);
     Serial.print(",");
     Serial.println(concentration);*/
-    Serial.print("Particles:");
-    Serial.print("    ");
-    Serial.println(concentration);
+    //Serial.print("Particles:");
+    //Serial.print("    ");
+    //Serial.println(concentration);
     lowpulseoccupancy = 0;
     starttime = millis();
   }
@@ -155,6 +175,69 @@ void lightBlueLed(){
       digitalWrite(yellow_led_pin, LOW);  
       digitalWrite(blue_led_pin, HIGH);  
 }
+
+void displayResults(){
+
+  char buf[BuffSize];
+  String str;
+  
+   u8g.firstPage();  
+  do {
+      int index = 0;
+      u8g.setFont(u8g_font_profont12);
+      index = u8g.drawStr( index, 10, "T: ");
+      if(temp>=0){
+          index += u8g.drawStr( index, 10, dtostrf(temp, 4, 1, buf));
+      }else{
+          index += u8g.drawStr( index, 10, dtostrf(temp, 5, 1, buf));
+      }
+      index += u8g.drawStr( index, 10, "\260C");
+      index+=5;//espace en Temp et Hum
+      
+      index += u8g.drawStr( index, 10, "Hum: ");
+      index += u8g.drawStr( index, 10, dtostrf(hum, 2, 0, buf));
+      index += u8g.drawStr( index, 10, "%");
+      index = 0;
+
+      u8g.setFont(u8g_font_profont15);
+      index += u8g.drawStr( index, 24, "Ressentie: ");
+      if(temp>=0){
+           index += u8g.drawStr( index, 24, dtostrf(tempRes, 4, 1, buf));
+      }else{
+           index += u8g.drawStr( index, 24, dtostrf(tempRes, 5, 1, buf));
+      }
+      index += u8g.drawStr( index, 24, "\260C");
+
+      index = 0;
+      u8g.setFont(u8g_font_profont12);
+      index += u8g.drawStr( index, 35, "Q.Air: ");
+      if (current_quality==0){
+        index += u8g.drawStr( index, 35, "Danger!!!");
+      }
+      else if (current_quality==1){
+        index += u8g.drawStr( index, 35, "Mauvaise");
+      }
+      else if (current_quality==2){
+        index += u8g.drawStr( index, 35, "Correcte");
+      }
+      else if (current_quality ==3){
+        index += u8g.drawStr( index, 35, "Bonne");
+      }
+      index += u8g.drawStr( index, 35, "(");
+      snprintf (buf, BuffSize, "%d", air_quality_value);
+      index += u8g.drawStr( index, 35, buf);
+      index += u8g.drawStr( index, 35, ")");
+
+      index = 0;
+      u8g.setFont(u8g_font_profont12);
+      index += u8g.drawStr( index, 46, "Conc.Particules: ");
+      index = 0;
+      str = String(concentration);
+      str.toCharArray(buf, BuffSize);
+      index += u8g.drawStr( index, 57,buf);
+  } while( u8g.nextPage() );
+}
+
 
 ISR(TIMER2_OVF_vect)
 {
